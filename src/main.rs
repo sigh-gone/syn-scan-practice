212a5449d88daf07bf595ff37d2b4dba2bd91b29
sigh-gone <a1@Als-MacBook-Pro.local>
2023-05-14 17:59:46 -0500

    changes to sending packets and receiving packets

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,113 @@
 use pnet::datalink::{Channel, NetworkInterface}; //{self, Channel, DataLinkReceiver, MacAddr, NetworkInterface};
 use pnet::packet::ethernet::{EtherTypes, EthernetPacket}; //, MutableEthernetPacket};
 use pnet::packet::ip::IpNextHeaderProtocols;
-use pnet::packet::ipv4::Ipv4Packet; //, MutableIpv4Packet, Ipv4Flags};
+use pnet::packet::ipv4::Ipv4Packet;
+use pnet::packet::ipv6::Ipv6Packet; //, MutableIpv4Packet, Ipv4Flags};
                                     //use pnet::packet::ipv6::MutableIpv6Packet;
 use pnet::packet::tcp::{MutableTcpPacket, TcpFlags, TcpPacket}; //TcpOption
 use pnet::packet::Packet;
-use pnet::transport::{self, TransportChannelType, TransportProtocol}; //transport_channel,};
-use pnet_packet::tcp::ipv4_checksum;
+use pnet::transport::{
+    self, TransportChannelType, TransportProtocol, TransportReceiver, TransportSender,
+}; //transport_channel,};
+use pnet_packet::tcp::{ipv4_checksum, ipv6_checksum, TcpOption};
 use rand::{thread_rng, Rng};
-use std::net::{IpAddr, Ipv4Addr}; //, Ipv6Addr};
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr}; //, Ipv6Addr};
+pub const IPV4_HEADER_LEN: usize = 20;
+pub const IPV6_HEADER_LEN: usize = 40;
+pub const ETHERNET_HEADER_LEN: usize = 14;
 
 #[tokio::main]
 async fn main() -> Result<(), String> {
     let mut rng = thread_rng();
     let port: u16 = rng.gen_range(1024..65535);
-    let public_ip = public_ip::addr().await.unwrap();
-    let pi = public_ip.to_string().parse::<Ipv4Addr>().unwrap();
-    let dest = "99.86.91.111".parse::<Ipv4Addr>().unwrap();
-    let packet = build_syn_packet(pi, dest, port, 443, 0);
+    let dest = "99.86.91.111".parse::<IpAddr>().unwrap();
+    let pi = "192.168.1.3".parse::<IpAddr>().unwrap();
+    let ports: Vec<u16> = vec![80, 443, 100];
+    let mut socket = get_socket(dest).unwrap();
 
-    send_packet(packet, dest);
-    receive_packets(port, 16);
+    send_packets_ipv4(&mut socket, ports, port, dest, pi);
+    receive_packets_("en0", port);
 
     Ok(())
 }
 
-fn send_packet(packet: Vec<u8>, dest_ip: std::net::Ipv4Addr) {
+fn get_socket(destination: IpAddr) -> Result<TransportSender, String> {
+    match destination.clone() {
+        IpAddr::V4(_) => {
+            let (ts, mut _tr) = transport::transport_channel(
+                4096,
+                TransportChannelType::Layer4(TransportProtocol::Ipv4(IpNextHeaderProtocols::Tcp)),
+            )
+            .unwrap();
+            Ok(ts)
+        }
+        IpAddr::V6(_) => {
+            let (ts, mut _tr) = transport::transport_channel(
+                4096,
+                TransportChannelType::Layer4(TransportProtocol::Ipv6(IpNextHeaderProtocols::Tcp)),
+            )
+            .unwrap();
+            Ok(ts)
+        }
+    }
+}
+
+fn send_packets_ipv4(
+    ts: &mut TransportSender,
+    ports: Vec<u16>,
+    source_port: u16,
+    dest_ip: std::net::IpAddr,
+    source_ip: std::net::IpAddr,
+) {
+    for dest_port in ports {
+        let mut vec: Vec<u8> = vec![0; 66];
+        let mut tcp_packet =
+            MutableTcpPacket::new(&mut vec[(ETHERNET_HEADER_LEN + IPV4_HEADER_LEN)..]).unwrap();
+        build_syn_packet(&mut tcp_packet, source_ip, dest_ip, source_port, dest_port);
+        let tcp_packet = TcpPacket::new(&tcp_packet.packet()).unwrap();
+        let _ = ts.send_to(tcp_packet, dest_ip.to_string().parse::<IpAddr>().unwrap());
+    }
+}
+
+#[warn(dead_code)]
+fn send_packets_ipv6(
+    ts: &mut TransportSender,
+    ports: Vec<u16>,
+    source_port: u16,
+    dest_ip: std::net::IpAddr,
+    source_ip: std::net::IpAddr,
+) {
+    for dest_port in ports {
+        let mut vec: Vec<u8> = vec![0; 86];
+        let mut tcp_packet =
+            MutableTcpPacket::new(&mut vec[(ETHERNET_HEADER_LEN + IPV6_HEADER_LEN)..]).unwrap();
+        build_syn_packet(&mut tcp_packet, source_ip, dest_ip, source_port, dest_port);
+        let tcp_packet = TcpPacket::new(&tcp_packet.packet()).unwrap();
+        let _ = ts.send_to(tcp_packet, dest_ip.to_string().parse::<IpAddr>().unwrap());
+    }
+}
+
+/*fn send_ipv4(ports: Vec<u16>, s_port: u16, destination: Ipv4Addr, source: Ipv4Addr) {
+    let (mut ts, mut _tr) = transport::transport_channel(
+        4096,
+        TransportChannelType::Layer4(TransportProtocol::Ipv4(IpNextHeaderProtocols::Tcp)),
+    )
+    .unwrap();
+    for port in ports {
+        let mut vec: Vec<u8> = vec![0; 66];
+        let mut tcp_packet =
+            MutableTcpPacket::new(&mut vec[(ETHERNET_HEADER_LEN + IPV4_HEADER_LEN)..]).unwrap();
+        build_syn_packet(&mut tcp_packet, source, destination, s_port, port, 0);
+        let tcp_packet = TcpPacket::new(&tcp_packet.packet()).unwrap();
+        let _ = ts.send_to(
+            tcp_packet,
+            destination.to_string().parse::<IpAddr>().unwrap(),
+        );
+    }
+}*/
+
+/*
+fn send_packet(packet: MutableTcpPacket, dest_ip: std::net::IpAddr) {
     let (mut ts, mut _tr) = transport::transport_channel(
         4096,
         TransportChannelType::Layer4(TransportProtocol::Ipv4(IpNextHeaderProtocols::Tcp)),
@@ -33,26 +115,16 @@ fn send_packet(packet: Vec<u8>, dest_ip: std::net::Ipv4Addr) {
     .unwrap();
 
     //transport::send_to(&mut ts, packet, std::net::IpAddr::V4(source_ip), std::net::IpAddr::V4(dest_ip)).unwrap();
-    let arr = demo(packet);
-    let tcp_packet = TcpPacket::new(&arr).unwrap();
+
+    let tcp_packet = TcpPacket::new(&packet.packet()).unwrap();
     let _ = ts.send_to(tcp_packet, dest_ip.to_string().parse::<IpAddr>().unwrap());
 }
+*/
 
-fn demo<T>(v: Vec<T>) -> [T; 20]
-where
-    T: Copy,
-{
-    let slice = v.as_slice();
-    let array: [T; 20] = match slice.try_into() {
-        Ok(ba) => ba,
-        Err(_) => panic!("Expected a Vec of length {} but it was {}", 32, v.len()),
-    };
-    array
-}
-
-fn receive_packets(s_port: u16, count: i32) {
+/*
+fn receive_packets(interface_name: &str, s_port: u16, count: i32) {
     let interfaces = pnet::datalink::interfaces();
-    let interfaces_name_match = |iface: &NetworkInterface| iface.name == "en0";
+    let interfaces_name_match = |iface: &NetworkInterface| iface.name == interface_name;
 
     let interface = interfaces.into_iter().find(interfaces_name_match).unwrap();
 
@@ -85,8 +157,10 @@ fn receive_packets(s_port: u16, count: i32) {
                             // process the SYN-ACK packet here
                             println!("{:?}", tcp_packet.get_source()); //.get_destination());
                             println!("in");
-                        } else {
-                            // println!("{:?}", tcp_packet.get_flags());
+                        } else if tcp_packet.get_destination() == s_port
+                            && tcp_packet.get_flags() == TcpFlags::RST
+                        {
+                            println!("closed");
                         }
                     }
                 }
@@ -95,29 +169,146 @@ fn receive_packets(s_port: u16, count: i32) {
         }
     }
 }
+*/
 
-fn build_syn_packet<'a>(
+fn receive_packets_(interface_name: &str, s_port: u16) {
+    let interfaces = pnet::datalink::interfaces();
+    let interfaces_name_match = |iface: &NetworkInterface| iface.name == interface_name;
+    let interface = interfaces.into_iter().find(interfaces_name_match).unwrap();
+
+    /*let iface_ip = match interface
+        .ips
+        .iter()
+        .nth(0)
+        .unwrap_or_else(|| panic!("the interface {} does not have any IP addresses", interface))
+        .ip()
+    {
+        IpAddr::V4(ipv4) => ipv4,
+        _ => panic!("ERR - Interface IP is IPv6 (or unknown) which is not currently supported"),
+    };*/
+
+    let (mut _t, mut rx) = match pnet::datalink::channel(&interface, Default::default()) {
+        Ok(Channel::Ethernet(tx, rx)) => (tx, rx),
+        Ok(_) => panic!("Unknown channel type"),
+        Err(e) => panic!("Error happened {}", e),
+    };
+    loop {
+        match rx.next() {
+            Ok(packet) => {
+                let eth_packet = EthernetPacket::new(packet).unwrap();
+                if eth_packet.get_ethertype() == EtherTypes::Ipv4 {
+                    let ipv4_packet = Ipv4Packet::new(eth_packet.payload()).unwrap();
+                    if ipv4_packet.get_next_level_protocol() == IpNextHeaderProtocols::Tcp {
+                        let tcp_packet = TcpPacket::new(ipv4_packet.payload()).unwrap();
+                        if tcp_packet.get_flags() == TcpFlags::SYN | TcpFlags::ACK
+                            && tcp_packet.get_destination() == s_port
+                        {
+                            // process the SYN-ACK packet here
+                            println!("{:?}", tcp_packet.get_source()); //.get_destination());
+                            println!("in");
+                        } else if tcp_packet.get_destination() == s_port
+                            && tcp_packet.get_flags() == TcpFlags::RST
+                        {
+                            println!("closed");
+                        }
+                    }
+                } else if eth_packet.get_ethertype() == EtherTypes::Ipv6 {
+                    let ipv6_packet = Ipv6Packet::new(eth_packet.payload()).unwrap();
+                    if ipv6_packet.get_next_header() == IpNextHeaderProtocols::Tcp {
+                        let tcp_packet = TcpPacket::new(ipv6_packet.payload()).unwrap();
+                        if tcp_packet.get_flags() == TcpFlags::SYN | TcpFlags::ACK
+                            && tcp_packet.get_destination() == s_port
+                        {
+                            // process the SYN-ACK packet here
+                            println!("{:?}", tcp_packet.get_source()); //.get_destination());
+                            println!("in");
+                        } else if tcp_packet.get_destination() == s_port
+                            && tcp_packet.get_flags() == TcpFlags::RST
+                        {
+                            println!("closed");
+                        }
+                    }
+                }
+            }
+            Err(e) => println!("error while receiving packet: {:?}", e),
+        }
+
+    }
+}
+
+/*
+fn build_syn_packet_<'a>(
+    tcp_packet: &mut MutableTcpPacket,
     source_ip: std::net::Ipv4Addr,
     dest_ip: std::net::Ipv4Addr,
     source_port: u16,
     dest_port: u16,
     seq_number: u32,
-) -> Vec<u8> {
-    let mut tcp_buffer = [0u8; 20];
-    let mut tcp_packet = MutableTcpPacket::new(&mut tcp_buffer).unwrap();
+) {
+    //let mut tcp_buffer = [0u8; 20];
+    //let mut tcp_packet = MutableTcpPacket::new(&mut tcp_buffer).unwrap();
 
     tcp_packet.set_source(source_port);
     tcp_packet.set_destination(dest_port);
     tcp_packet.set_sequence(seq_number);
     tcp_packet.set_flags(TcpFlags::SYN);
     tcp_packet.set_window(64240);
-    tcp_packet.set_data_offset(5);
-    tcp_packet.set_checksum(0);
+    tcp_packet.set_data_offset(8);
+    tcp_packet.set_urgent_ptr(0);
+    tcp_packet.set_sequence(0);
+    tcp_packet.set_options(&[
+        TcpOption::mss(1460),
+        TcpOption::sack_perm(),
+        TcpOption::nop(),
+        TcpOption::nop(),
+        TcpOption::wscale(7),
+    ]);
 
     let checksum = ipv4_checksum(&tcp_packet.to_immutable(), &source_ip, &dest_ip);
     tcp_packet.set_checksum(checksum);
+}
+*/
 
-    Vec::from(tcp_packet.packet())
+fn build_syn_packet<'a>(
+    tcp_packet: &mut MutableTcpPacket,
+    source_ip: std::net::IpAddr,
+    dest_ip: std::net::IpAddr,
+    source_port: u16,
+    dest_port: u16,
+) {
+    //let mut tcp_buffer = [0u8; 20];
+    //let mut tcp_packet = MutableTcpPacket::new(&mut tcp_buffer).unwrap();
+
+    tcp_packet.set_source(source_port);
+    tcp_packet.set_destination(dest_port);
+    tcp_packet.set_sequence(0);
+    tcp_packet.set_flags(TcpFlags::SYN);
+    tcp_packet.set_window(64240);
+    tcp_packet.set_data_offset(8);
+    tcp_packet.set_urgent_ptr(0);
+    tcp_packet.set_sequence(0);
+    tcp_packet.set_options(&[
+        TcpOption::mss(1460),
+        TcpOption::sack_perm(),
+        TcpOption::nop(),
+        TcpOption::nop(),
+        TcpOption::wscale(7),
+    ]);
+
+    match source_ip {
+        IpAddr::V4(src) => {
+            if let Ok(dest_ip) = dest_ip.to_string().parse::<Ipv4Addr>() {
+                let checksum = ipv4_checksum(&tcp_packet.to_immutable(), &src, &dest_ip);
+                tcp_packet.set_checksum(checksum);
+            }
+        }
+        IpAddr::V6(src) => {
+            if let Ok(dest_ip) = dest_ip.to_string().parse::<Ipv6Addr>() {
+                let checksum = ipv6_checksum(&tcp_packet.to_immutable(), &src, &dest_ip);
+                tcp_packet.set_checksum(checksum);
+            }
+        }
+    }
 }
 
 /*
