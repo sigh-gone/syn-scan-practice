824d851999fa593af34070d2c97a480c7b29261e
sigh-gone <a1@Als-MacBook-Pro.local>
2023-05-13 20:00:24 -0500

    added ipv6 packet building

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,40 @@
-use std::net::{IpAddr, Ipv4Addr};
-
-use pnet::datalink;
 use pnet::datalink::{Channel, MacAddr, NetworkInterface};
 use pnet::packet::ethernet::{EtherTypes, MutableEthernetPacket};
-use pnet::packet::ip::{IpNextHeaderProtocol, IpNextHeaderProtocols};
+use pnet::packet::ip::IpNextHeaderProtocols;
 use pnet::packet::ipv4::Ipv4Flags;
 use pnet::packet::ipv4::MutableIpv4Packet;
 use pnet::packet::tcp::TcpOption;
 use pnet::packet::tcp::{MutableTcpPacket, TcpFlags};
-use pnet::packet::{MutablePacket, Packet, PacketSize};
-use pnet::transport::{self, TransportSender};
-use pnet::transport::{transport_channel, TransportChannelType, TransportProtocol};
-use pnet::util::checksum;
-use pnet_packet::ethernet::{EtherType, EthernetPacket};
-use pnet_packet::ip::IpNextHeaderProtocols::Tcp;
+use pnet::packet::Packet;
+use pnet_packet::ethernet::EthernetPacket;
 use pnet_packet::ipv4::Ipv4Packet;
+use pnet_packet::ipv6::MutableIpv6Packet;
 use pnet_packet::tcp::TcpPacket;
-use std::net::TcpStream;
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 
 fn main() {
     println!("Hello, world!");
 }
 
-pub fn build_random_packet(partial_packet: &PartialTCPPacketData, tmp_packet: &mut [u8]) {
+pub struct PartialIpv4TCPPacketData<'a> {
+    pub destination_ip: Ipv4Addr,
+    pub iface_ip: Ipv4Addr,
+    pub iface_name: &'a String,
+    pub iface_src_mac: &'a MacAddr,
+    pub sport: &'a u16,
+    pub dport: &'a u16,
+}
+
+pub struct PartialIpv6TCPPacketData<'a> {
+    pub destination_ip: Ipv6Addr,
+    pub iface_ip: Ipv6Addr,
+    pub iface_name: &'a String,
+    pub iface_src_mac: &'a MacAddr,
+    pub sport: &'a u16,
+    pub dport: &'a u16,
+}
+
+pub fn build_random_ipv4(partial_packet: &PartialIpv4TCPPacketData, tmp_packet: &mut [u8]) {
     const ETHERNET_HEADER_LEN: usize = 14;
     const IPV4_HEADER_LEN: usize = 20;
 
@@ -62,8 +74,8 @@ pub fn build_random_packet(partial_packet: &PartialTCPPacketData, tmp_packet: &m
             MutableTcpPacket::new(&mut tmp_packet[(ETHERNET_HEADER_LEN + IPV4_HEADER_LEN)..])
                 .unwrap();
 
-        tcp_header.set_source(rand::random::<u16>());
-        tcp_header.set_destination(rand::random::<u16>());
+        tcp_header.set_source(*partial_packet.sport);
+        tcp_header.set_destination(*partial_packet.dport);
 
         tcp_header.set_flags(TcpFlags::SYN);
         tcp_header.set_window(64240);
@@ -88,14 +100,68 @@ pub fn build_random_packet(partial_packet: &PartialTCPPacketData, tmp_packet: &m
     }
 }
 
-pub struct PartialTCPPacketData<'a> {
-    pub destination_ip: Ipv4Addr,
-    pub iface_ip: Ipv4Addr,
-    pub iface_name: &'a String,
-    pub iface_src_mac: &'a MacAddr,
+pub fn build_random_ipv6(partial_packet: &PartialIpv6TCPPacketData, tmp_packet: &mut [u8]) {
+    const ETHERNET_HEADER_LEN: usize = 14;
+    const IPV4_HEADER_LEN: usize = 40;
+
+    // Setup Ethernet header
+    {
+        let mut eth_header =
+            MutableEthernetPacket::new(&mut tmp_packet[..ETHERNET_HEADER_LEN]).unwrap();
+
+        eth_header.set_destination(MacAddr::broadcast());
+        eth_header.set_source(*partial_packet.iface_src_mac);
+        eth_header.set_ethertype(EtherTypes::Ipv4);
+    }
+
+    // Setup IP header
+    {
+        let mut ip_header = MutableIpv6Packet::new(
+            &mut tmp_packet[ETHERNET_HEADER_LEN..(ETHERNET_HEADER_LEN + IPV4_HEADER_LEN)],
+        )
+        .unwrap();
+
+        ip_header.set_version(6);
+        ip_header.set_flow_label(0);
+        ip_header.set_payload_length(0);
+        ip_header.set_next_header(IpNextHeaderProtocols::Tcp);
+        ip_header.set_hop_limit(64);
+        ip_header.set_source(partial_packet.iface_ip);
+        ip_header.set_destination(partial_packet.destination_ip);
+    }
+
+    // Setup TCP header
+    {
+        let mut tcp_header =
+            MutableTcpPacket::new(&mut tmp_packet[(ETHERNET_HEADER_LEN + IPV4_HEADER_LEN)..])
+                .unwrap();
+
+        tcp_header.set_source(*partial_packet.sport);
+        tcp_header.set_destination(*partial_packet.dport);
+        tcp_header.set_flags(TcpFlags::SYN);
+        tcp_header.set_window(64240);
+        tcp_header.set_data_offset(8);
+        tcp_header.set_urgent_ptr(0);
+        tcp_header.set_sequence(0);
+
+        tcp_header.set_options(&[
+            TcpOption::mss(1460),
+            TcpOption::sack_perm(),
+            TcpOption::nop(),
+            TcpOption::nop(),
+            TcpOption::wscale(7),
+        ]);
+
+        let checksum = pnet::packet::tcp::ipv6_checksum(
+            &tcp_header.to_immutable(),
+            &partial_packet.iface_ip,
+            &partial_packet.destination_ip,
+        );
+        tcp_header.set_checksum(checksum);
+    }
 }
 
-pub fn send_tcp_packets(destination_ip: Ipv4Addr, interface: String, count: u32) {
+pub fn send_tcp_ipv4(destination_ip: Ipv4Addr, interface: String, count: u32) {
     let interfaces = pnet::datalink::interfaces();
 
     println!("List of Available Interfaces\n");
@@ -135,7 +201,7 @@ pub fn send_tcp_packets(destination_ip: Ipv4Addr, interface: String, count: u32)
         _ => panic!("ERR - Interface IP is IPv6 (or unknown) which is not currently supported"),
     };
 
-    let partial_packet: PartialTCPPacketData = PartialTCPPacketData {
+    let partial_packet: PartialIpv4TCPPacketData = PartialIpv4TCPPacketData {
         destination_ip: destination_ip,
         iface_ip,
         iface_name: &interface.name,
@@ -151,7 +217,7 @@ pub fn send_tcp_packets(destination_ip: Ipv4Addr, interface: String, count: u32)
     //v6 0x86DD
     //v4 2048
     tx.build_and_send(1, 66, &mut |packet: &mut [u8]| {
-        build_random_packet(&partial_packet, packet);
+        build_random_ipv4(&partial_packet, packet);
     });
     if let Ok(packet) = rx.next() {
         let eth_packet = EthernetPacket::new(packet).unwrap();
@@ -165,7 +231,10 @@ pub fn send_tcp_packets(destination_ip: Ipv4Addr, interface: String, count: u32)
                     // Extract the TCP segment
                     if let Some(tcp_segment) = TcpPacket::new(ipv4_packet.payload()) {
                         // Do something with the TCP segment
-                        if tcp_segment.get_flags() == TcpFlags::SYN + TcpFlags::ACK {
+                        println!("{:?}", tcp_segment.get_flags());
+                        if tcp_segment.get_destination() == *partial_packet.sport
+                            && tcp_segment.get_flags() == TcpFlags::SYN + TcpFlags::ACK
+                        {
                             println!(
                                 "Received TCP packet with source port {}, destination port {}",
                                 tcp_segment.get_source(),
