d6f8046e22eacc15316790ff5d177e059d8f6517
Al <112661044+sigh-gone@users.noreply.github.com>
2023-07-28 10:50:29 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ pub const ETHERNET_HEADER_LEN: usize = 14;
 fn main() {
     let mut rng = thread_rng();
     let port: u16 = rng.gen_range(10024..65535);
-    let dest = "99.86.91.111".parse::<IpAddr>().unwrap();
+    let dest = "127.0.0.111".parse::<IpAddr>().unwrap();
     let ports: Vec<u16> = vec![80, 443, 100];
     let mut socket = get_socket(dest).unwrap();
     let (interface, iface_ip) = get_interface("en0");
